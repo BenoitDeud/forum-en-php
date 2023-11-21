2d8199c8de4ab623f4a404d678d63e8b29d7d937
Benoit Deudon <deudon.benoit1@outlook.fr>
2023-11-21 15:34:04 +0100

    formulaire ready

--- controllers/addControllerForm.php
+++ controllers/addControllerForm.php
@@ -1,28 +1,85 @@
-<?php require_once "../includes/connexiondb.php";
+<?php
 
+// début de la session pour mettre les indications en cas de réussite ou d'erreur
+session_start();
+
+require_once "../includes/connexiondb.php";
+
+// connexion base de donnée
 $con = connectdb();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    // récupération des variables
     $nom = $_POST["nom"];
     $prenom = $_POST["prenom"];
     $mail = $_POST["mail"];
-    $regexMail = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/';
     $mot = $_POST["mdps"];
-    password_hash($mot, PASSWORD_DEFAULT);
-    $regexPassword = '/^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[#+-^[]])$/';
+    $cMot = $_POST["cMdps"];
 
-    if (preg_match($regex, $mail)) {
-        echo $email . " OUI.";
-        } else { 
-        echo $email . " NON.";
-        } 
-    
-    $req = $con->prepare('INSERT INTO utilisateur (nomUtilisateur,prenomUtilisateur,adresseMail,motPasse) VALUES (?,?,?,?)');
+    // regex du nom et prénom
+    $regexNomPre = '/^([a-zA-Zéèîïô]){3,}$/i';
 
-    $req->execute(array($nom, $prenom, $mail, $mot));
+    // regex de l'adresse mail
+    $regexMail = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
 
-    header('Location:../views/login.php');
-    exit;
+    // regex mot de passe 
+    $regexPass = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[#\^\+\-\[\]])(?=.{8,})/';
 
+    // voir si l'adresse mail existe déjà dans la base de donnée avec une requête préparée
+    $mailOnlyOne = $con->prepare('SELECT * FROM utilisateur WHERE adresseMail=?');
+    $mailOnlyOne->execute([$mail]);
+    $utilisateurMail = $mailOnlyOne->fetch();
+
+    // hachage mot de passe
+    $steak = password_hash($mot, PASSWORD_DEFAULT);
+
+    if (!preg_match($regexNomPre, $nom) || $nom == "") {
+        $_SESSION['erreur1'] = 1;
+        header("Refresh:0; url= ../views/form.php");
+
+    } else if (!preg_match($regexNomPre, $prenom) || $prenom == "") {
+        $_SESSION['erreur2'] = 1;
+        header("Refresh:0; url= ../views/form.php");
+
+    } else if (!preg_match($regexMail, $mail) || $mail == "") {
+        $_SESSION['erreur3'] = 1;
+        header("Refresh:0; url= ../views/form.php");
+
+    } else if ($utilisateurMail) {
+        $_SESSION['erreur4'] = 1;
+        header("Refresh:0; url= ../views/form.php");
+
+    } else if (!preg_match($regexPass, $mot) || $mot == "") {
+        $_SESSION['erreur5'] = 1;
+        header("Refresh:0; url= ../views/form.php");
+
+    } else if ($mot != $cMot) {
+        $_SESSION['erreur6'] = 1;
+        header("Refresh:0; url= ../views/form.php");
+
+    } else {
+        $req = $con->prepare("INSERT INTO utilisateur (nomUtilisateur,prenomUtilisateur,adresseMail,motPasse) VALUES (?,?,?,?)");
+        $req->execute(array($nom, $prenom, $mail, $steak, ));
+        $_SESSION['erreur1'] = 0;
+        $_SESSION['erreur2'] = 0;
+        $_SESSION['erreur3'] = 0;
+        $_SESSION['erreur4'] = 0;
+        $_SESSION['erreur5'] = 0;
+        $_SESSION['erreur6'] = 0;
+        $_SESSION['success'] = 1;
+        header('Location:../views/form.php');
+        exit;
+    }
+}
+if (isset($_POST['reset'])) {
+	$_SESSION['erreur1'] = 0;
+	$_SESSION['erreur2'] = 0;
+	$_SESSION['erreur3'] = 0;
+	$_SESSION['erreur4'] = 0;
+	$_SESSION['erreur5'] = 0;
+	$_SESSION['erreur6'] = 0;
+	$_SESSION['success'] = 0;
+	header("Refresh:0; url= ../views/form.php");
 }
 ?>
--- views/form.php
+++ views/form.php
@@ -11,7 +11,14 @@
     <header>
         <div class="flexForum2">
             <p>Inscription</p><br>
+            <?php
+            if ($_SESSION['success'] == 1) {
+                echo '<p id="success">Kebab !</p>';
+            }
+            ?>
             <a href="../index.php">Accueil</a>
+            <a href="../views/login.php">Login</a>
+
         </div>
     </header>
 
@@ -20,17 +27,35 @@
 
         <div class="form">
             <label for="nom">Votre nom (3 caractères minimum, pas de chiffre) : <span id="error"></span></label>
+            <?php
+            if ($_SESSION['erreur1'] == 1) {
+                echo '<p id="erreur">Nom incorrect ou trop court (3 caractères minimum et sans chiffres) !</p>';
+            } 
+            ?>
             <input type="text" name="nom" placeholder="Martin" id="nom">
         </div>
 
         <div class="form">
             <label for="prenom">Votre prénom (3 caractères minimum, pas de chiffre) :<span
                     id="errorDeux"></span></label>
+            <?php
+            if ($_SESSION['erreur2'] == 1) {
+                echo '<p id="erreur">Prénom incorrect ou trop court (3 caractères minimum et sans chiffres) !</p>';
+            } 
+            ?>
             <input type="text" name="prenom" placeholder="Matin" id="prenom">
         </div>
 
         <div class="form">
             <label for="mail">Votre adresse mail :<span id="errorTrois"></span></label>
+            <?php
+            if ($_SESSION['erreur3'] == 1) {
+                echo '<p id="erreur">Pas une email</p>';
+            }
+            if ($_SESSION['erreur4'] == 1) {
+                echo '<p id="erreur">Le mail est déjà dans notre base de données, il en faut une autre</p>';
+            } 
+            ?>
             <input type="text" name="mail" id="mail" placeholder="bbbbbbbb@bbbbbbb.bb">
         </div>
 
@@ -38,20 +63,31 @@
             <label for="mdps">Votre mot de passe (doit contenir 8 caractères, une maj, une min, un chiffre et un
                 caractère spécial
                 (#+-^[])) :<span id="errorQuatre"></span></label>
+            <?php
+            if ($_SESSION['erreur5'] == 1) {
+                echo '<p id="erreur">tu peux le faire !, ne pas oublier : (#+-^[])</p>';
+            }
+            ?>
             <input type="text" name="mdps" id="mdps" placeholder="**************">
         </div>
 
         <div class="form">
             <label for="cMdps">Confirmez votre mot de passe :<span id="errorCinq"></span></label>
+            <?php
+            if ($_SESSION['erreur6'] == 1) {
+                echo '<p id="erreur">Le mot de passe est différent !!!</p>';
+            }
+            ?>
             <input type="text" name="cMdps" id="cMdps" placeholder="**************">
         </div>
 
-        <input type="submit" value="S'enregistrer" id="submit">
+        <input type="submit" value="S'enregistrer" id="submit"><br>
+        <input id="submit" class="input-style" type="submit" name="reset" value="Reset" />
 
     </form>
-
+    <!--
     <script src="../JAVASCRIPT/script.js"></script>
-
+-->
 </body>
 
 </html>
